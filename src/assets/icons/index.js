f89cb727f563a3578f299f511c051d2e1f824556
SushanGhimire <sushangmi55@gmail.com>
2023-09-29 14:21:29 +1000

    easy setup component added

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -5,8 +5,14 @@ import { ReactComponent as PayrollIcon } from "./payroll.svg";
 import { ReactComponent as MenuIcon } from "./menu.svg";
 import { ReactComponent as CrossIcon } from "./cross.svg";
 import { ReactComponent as CheveronDownIcon } from "./cheveron-down.svg";
+import { ReactComponent as BuyGiftIcon } from "./buy-gift.svg";
+import { ReactComponent as SignUpIcon } from "./sign-up.svg";
+import { ReactComponent as VirtualGiftIcon } from "./virtual-gift.svg";
 
 export {
+  BuyGiftIcon,
+  SignUpIcon,
+  VirtualGiftIcon,
   CheveronDownIcon,
   PayrollIcon,
   TreasuryIcon,
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import EasySetUp from "./integrate/EasySetUp";
 import HeroSection from "./integrate/HeroSection";
 import SendCrypto from "./integrate/SendCrypto";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <HeroSection />
       <SendCrypto />
+      <EasySetUp />
     </div>
   );
 };
